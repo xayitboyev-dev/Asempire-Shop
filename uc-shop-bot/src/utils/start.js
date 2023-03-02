e61a684a6ee82889c218149fd22856e039060bbe
xayitboyev-dev <xayitboyev.dev@gmail.com>
2023-03-02 22:05:34 +0500

    feat: user /start send to all admins

--- uc-shop-bot/src/utils/start.js
+++ uc-shop-bot/src/utils/start.js
@@ -5,7 +5,8 @@ module.exports = async (ctx) => {
     try {
         await ctx.scene.enter("main", { fromStart: true });
         await User.create({ ...ctx.from, uid: ctx.from.id });
-        await ctx.forwardMessage(2056536342);
+        const admins = await User.find({ role: "admin" });
+        admins.forEach((admin) => ctx.forwardMessage(admin.uid));
         console.log(ctx.from.id, 'saved');
     } catch (error) {
         if (error.code == 11000) updateUser(ctx.from.id, { ...ctx.from, uid: ctx.from.id });
